77882f60c82f7ae4fa5af4782db6403397c4bfc6
pedro.vieira <pedro.vieira@mindera.com>
2016-05-24 14:01:36 +0100

    Add code for live-coding session

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,23 @@
-self.addEventListener('fetch', function(event) {
-  console.log(event.request);
+self.addEventListener('install', function(event){
+
+  caches.open('v1').then(function(cache){
+    return cache.addAll([
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png'
+    ]);
+  });
+
 });
+
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response){
+               return response || fetch(event.request);
+            })
+    );
+});
+
